054c19b7bc7a0941c9db3a50f6bfe2fca01bebd5
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2025-04-26 18:44:55 -0300

    feat: Aplicar principios do POO nos DTOs de entrada e saída

--- customer-management/src/main/java/br/edu/unipe/domain/address/dto/AddressInputDTO.java
+++ customer-management/src/main/java/br/edu/unipe/domain/address/dto/AddressInputDTO.java
@@ -1,17 +1,36 @@
 package br.edu.unipe.domain.address.dto;
 
+import br.edu.unipe.domain.address.State;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class AddressInputDTO {
     @NotBlank(message = "Street is required")
-    public String street;
-    public String complement;
-    @NotBlank(message = "City is required")
-    public String city;
+    private String street;
 
-    @NotBlank(message = "State is required")
-    public String state;
+    private String complement;
+
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NotNull(message = "State is required")
+    private State state;
 
     @NotBlank(message = "Postal code is required")
-    public String postalCode;
+    private String postalCode;
+
+    public String getStreet() { return street; }
+    public void setStreet(String street) { this.street = street; }
+
+    public String getComplement() { return complement; }
+    public void setComplement(String complement) { this.complement = complement; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public State getState() { return state; }
+    public void setState(State state) { this.state = state; }
+
+    public String getPostalCode() { return postalCode; }
+    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
 }
